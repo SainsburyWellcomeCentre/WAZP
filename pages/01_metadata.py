4b732541aa02fc8e0a28cee508d01b389acf9ca2
sfmig <33267254+sfmig@users.noreply.github.com>
2022-12-12 11:05:10 +0000

    add mock table to metadata page. start mock project

--- pages/01_metadata.py
+++ pages/01_metadata.py
@@ -3,16 +3,38 @@
 
 import dash
 from dash import html, dcc
+import dash_bootstrap_components as dbc
+import pandas as pd
 
+
+###############
+# Register this page
 dash.register_page(__name__)
 
+
+###############
+# Define table component from pandas dataframe
+df = pd.read_csv(
+    "https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv"
+)
+
+table = dbc.Table.from_dataframe(
+    df,
+    bordered=True, #dark=True, #deprecated µ~~~~ñ~~~ññ
+    hover=True,
+    responsive=True, # if True, table can be scrolled horizontally?
+    striped=True # applies zebra striping to the rows
+    )
+
+
+########################
+# Define layout
 layout = html.Div(
     children=[
-        html.H1(children="This is the Metadata page"),
+        html.H1(children="Metadata page"),
         html.Div(
-            children="""
-        This is the Metadata page content.
-    """
+            #children=""" This is the Metadata page content."""
+            dbc.Container(table, className="p-5")
         ),
     ]
 )
