23acad5201388de8724f12a8812917b3ce6cafe8
Sam Cunliffe <s.cunliffe@ucl.ac.uk>
2023-03-17 14:07:28 +0000

    Extend the "loading ok" smoke test to all tabs.
    Currently failing for genuine errors.

--- tests/test_integration/test_smoke.py
+++ tests/test_integration/test_smoke.py
@@ -1,17 +1,34 @@
+"""Smoke tests which start the server and check we can navigate without error."""
+import os
+import pytest
 from dash.testing.composite import DashComposite
 
 from wazp.app import app
 
 
-def test_start_server_no_errors(dash_duo: DashComposite) -> None:
-    """A minimal smoke test: launching the wazp webapp should startup, and
-    display the page content without error.
-
-    Parameters:
-        dash_duo: Default fixture for Dash Python integration tests.
-    """
+@pytest.mark.parametrize(
+    "page, title",
+    [
+        ("", "Home"),
+        ("/01-metadata", "Metadata"),
+        ("/02-roi", "ROI definition"),
+        ("/03-pose-estimation", "Pose estimation inference"),
+        ("/04-dashboard", "Dashboard"),
+    ],
+)
+def test_start_server_open_page_no_errors(
+    dash_duo: DashComposite, page: str, title: str
+) -> None:
     dash_duo.start_server(app)
-    dash_duo.wait_for_text_to_equal("h1", "Home", timeout=4)
-    assert (
-        dash_duo.get_logs() == []
-    ), "There are errors in the browser console!"
+    if page:
+        dash_duo.wait_for_page(dash_duo.server_url + page, timeout=4)
+    dash_duo.wait_for_text_to_equal("h1", title, timeout=4)
+
+    errors = dash_duo.get_logs()
+    if errors:
+        screenshots_dirname = ".test_fail_screenshots/smoke"
+        os.makedirs(screenshots_dirname, exist_ok=True)  # mkdir -p
+        dash_duo.driver.save_screenshot(
+            f"./{screenshots_dirname}/test-{dash_duo.session_id}{page.replace('/', '-')}.png"
+        )
+        assert False, f"There are {len(errors)} errors in the browser console!"
