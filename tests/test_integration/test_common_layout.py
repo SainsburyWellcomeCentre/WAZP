8cb7ebb608714fd102110ea470ad1c76b167be62
sfmig <33267254+sfmig@users.noreply.github.com>
2023-05-18 11:31:56 +0100

    parametrise test sidebar links

--- tests/test_integration/test_common_layout.py
+++ tests/test_integration/test_common_layout.py
@@ -1,4 +1,3 @@
-import dash
 import pytest
 import selenium
 from dash.testing.composite import DashComposite
@@ -47,20 +46,43 @@ def test_components_created(
     " in the browser console!"
 
 
-@pytest.mark.xfail(
-    raises=AssertionError,
-    reason=(
-        "Feature not yet implemented:"
-        "When config has not been loaded, "
-        "warnings should show in pages that are not Home"
-    ),
-    strict=True,
-    # with strict=True
-    # if the test passes unexpectedly,
-    # it will fail the test suite
+# @pytest.mark.xfail(
+#     raises=AssertionError,
+#     reason=(
+#         "Feature not yet implemented:"
+#         "When config has not been loaded, "
+#         "warnings should show in pages that are not Home"
+#     ),
+#     strict=True,
+#     # with strict=True
+#     # if the test passes unexpectedly,
+#     # it will fail the test suite
+# )
+@pytest.mark.parametrize(
+    "page_name",
+    [
+        pytest.param(
+            name,
+            marks=pytest.mark.xfail(
+                reason=(
+                    "Feature not yet implemented:"
+                    "When config has not been loaded, "
+                    "warnings should show in pages that are not Home"
+                )
+            ),
+        )
+        for name in [
+            "Home",
+            "01 metadata",
+            "02 roi",
+            "03 pose estimation",
+            "04 dashboard",
+        ]
+    ],
 )
 def test_sidebar_links(
     dash_duo: DashComposite,
+    page_name: str,
     map_page_name_to_title: dict,
     timeout: float,
 ) -> None:
@@ -70,6 +92,8 @@ def test_sidebar_links(
     Parameters:
         dash_duo : DashComposite
             Default fixture for Dash Python integration tests.
+        page : str
+            ....
         map_page_name_to_title: dictionary : dict
             dictionary with page names as keys, and page titles as values
         timeout : float
@@ -80,32 +104,28 @@ def test_sidebar_links(
     dash_duo.start_server(app)
 
     # click through links in sidebar
-    for page in dash.page_registry.values():
+    # for page in dash.page_registry.values():
 
-        # click thru each page
-        dash_duo.find_element(
-            "#sidebar #link-" + page["name"].replace(" ", "-"),
-        ).click()
+    # click thru each page
+    dash_duo.find_element(
+        "#sidebar #link-" + page_name.replace(" ", "-"),
+    ).click()
 
-        # check page title
-        try:
-            dash_duo.wait_for_text_to_equal(
-                "h1", map_page_name_to_title[page["name"]], timeout=timeout
-            )
+    # check page title
+    try:
+        dash_duo.wait_for_text_to_equal(
+            "h1", map_page_name_to_title[page_name], timeout=timeout
+        )
 
-        except selenium.common.exceptions.TimeoutException:
-            pytest.fail(
-                f'Timeout waiting for page {page["name"]} '
-                "to show a title with the text: "
-                f'{map_page_name_to_title[page["name"]]}'
-            )
+    except selenium.common.exceptions.TimeoutException:
+        pytest.fail(
+            f"Timeout waiting for page {page_name} "
+            "to show a title with the text: "
+            f"{map_page_name_to_title[page_name]}"
+        )
 
-        # click back to home
-        # NOTE: consider doing this via test parametrisation
-        dash_duo.find_element("#sidebar #link-Home").click()
-
-        # TODO: if no config file has been loaded, check a warning is shown?
-        # ...
+    # TODO: if no config file has been loaded, check a warning is shown?
+    # ...
 
     # NOTE: this is expected to fail
     assert (
