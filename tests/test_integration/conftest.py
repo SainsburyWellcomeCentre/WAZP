f14af91e0d4e5122a2388a48e8071dcde3fc8fd8
Sam Cunliffe <s.cunliffe@ucl.ac.uk>
2023-05-18 15:55:16 +0100

    Add a test helper pytest fixture.
    A way to have convenience helper functions for developing tests.
    @sfmig prefers a fixture.
    https://stackoverflow.com/a/42156088/1444054

--- tests/test_integration/conftest.py
+++ tests/test_integration/conftest.py
@@ -1,4 +1,49 @@
+import os
+
+import pytest
+from dash.testing.composite import DashComposite
 from selenium.webdriver.chrome.options import Options
+
+
+class Helpers:
+    """A class to group helpful things when writing tests.
+
+    Added as a pytest fixture.
+
+    Example
+    -------
+    ```
+    def test_my_thing(helpers)
+        helpers.help_me()
+    ```
+    """
+
+    @staticmethod
+    def debug_screenshot(dash_duo: DashComposite, name: str = "") -> None:
+        """Save a screenshot of the current state of the testing server.
+
+        Useful for debugging and developing tests.
+        Saves to .test_debug_screenshots or
+        .test_debug_screenshots/<name> if `name` is provided.
+
+        Parameters
+        ----------
+            dash_duo : DashComposite
+                The current dash_duo fixture.
+            name: str, optional
+                A name (to help you find the screenshots.)
+        """
+        screenshots_dirname = ".test_helper_screenshots/"
+        if name:
+            screenshots_dirname += f"{name}/"
+        os.makedirs(screenshots_dirname, exist_ok=True)  # mkdir -p
+        filename = f"test-{dash_duo.session_id}.png"
+        dash_duo.driver.save_screenshot(f"./{screenshots_dirname}/{filename}")
+
+
+@pytest.fixture
+def helpers():
+    return Helpers
 
 
 def pytest_setup_options():
