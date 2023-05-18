4026025fbe2e6e3a2605cbedc4a36eebe69039e6
Sam Cunliffe <s.cunliffe@ucl.ac.uk>
2023-05-18 16:26:42 +0100

    WIP

--- tests/test_integration/test_load_config.py
+++ tests/test_integration/test_load_config.py
@@ -1,9 +1,11 @@
+from time import sleep
+
 from dash.testing.composite import DashComposite
 
 from wazp.app import app
 
 
-def test_load_config_once(dash_duo: DashComposite) -> None:
+def test_load_config_once(dash_duo: DashComposite, helpers) -> None:
     """
     ...
 
@@ -11,7 +13,15 @@ def test_load_config_once(dash_duo: DashComposite) -> None:
         dash_duo: Default fixture for Dash Python integration tests.
     """
     dash_duo.start_server(app)
+
+    #
     dash_duo.wait_for_text_to_equal("h1", "Home", timeout=4)
-    assert (
-        dash_duo.get_logs() == []
-    ), "There are errors in the browser console!"
+    helpers.debug_screenshot(dash_duo)
+    upload_area = dash_duo.find_elements(
+        "Select project config file", attribute="PARTIAL_LINK_TEXT"
+    )
+    assert len(upload_area) == 1, "Couldn't find the upload button."
+    upload_area = upload_area[0]
+    upload_area.click()
+    # upload_area = "upload-project-config
+    dash_duo.multiple_click(upload_area, 1)
